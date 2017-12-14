5551f40bf32e0fc861009538696404ddad2f6fb1
paulokuong <pkuong80@gmail.com>
2017-12-13 22:14:18 -0500

    Added request for getting observation stations in a grid

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='noaa-sdk',
-      version='0.1.5',
+      version='0.1.6',
       description='NOAA API (V3) Python 3 SDK.',
       install_requires=[
           'httplib2==0.10.3',
