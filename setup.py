1ef706bb994e94c8f7abd9208478bbf0eecb5056
paulokuong <pkuong80@gmail.com>
2017-12-13 22:41:42 -0500

    updated version to 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='noaa-sdk',
-      version='0.1.6b',
+      version='0.1.7',
       description='NOAA API (V3) Python 3 SDK.',
       install_requires=[
           'httplib2==0.10.3',
