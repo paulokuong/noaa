f06057cad744742b2e83c22b0e56b693d4236db3
paulokuong <pkuong80@gmail.com>
2017-12-15 17:46:50 -0500

    fixed parsing issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='noaa-sdk',
-      version='0.1.8',
+      version='0.1.9',
       description='NOAA API (V3) Python 3 SDK.',
       install_requires=[
           'httplib2==0.10.3',
