38f5f77c369529c26d19a8b2391549f2d0aff1b2
Paulo-Kuong <paulo.kuong@ibm.com>
2018-04-04 10:32:19 -0400

    bump up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='noaa-sdk',
-      version='0.1.12c',
+      version='0.1.13',
       description='NOAA API (V3) Python 3 SDK.',
       install_requires=[
           'httplib2==0.10.3',
