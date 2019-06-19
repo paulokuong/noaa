05eb5c01d39a8f9f7571c701c97644c7ac593862
toppk <toppk@bllue.org>
2019-06-19 14:27:02 -0400

    exclude tests from packaging

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='noaa-sdk',
       author='Paulo Kuong',
       author_email='paulo.kuong@gmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests"]),
       include_package_data=True,
       zip_safe=False,
       long_description=long_description)
